09fdde8aba327d25afda52bf9db9e47726a7b2f0
Julian <49489282+juy13@users.noreply.github.com>
2019-11-15 01:59:29 +0300

    Add files via upload
    Added operations with constant char

---
+++ Laba.cpp
@@ -0,0 +1,81 @@
+﻿// Laba.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "SymbAm.h"
+
+
+
+
+int main()
+{
+	SymbAm S7('2');
+	std::cout << S7;
+	char z = 'z';
+	SymbAm S3, S4;
+	S3 = S4 + z;
+	std::cout << S3;
+	SymbAm S("01234");
+	SymbAm S1("ABC");
+	SymbAm S2 = S + S1;
+	std::cout << S ;
+	std::cout << S1;
+	std::cout << S2;
+
+
+
+	SymbAm A;
+	int i = 0;
+	std::cout << "Inputing elements in class A" << std::endl;
+	try
+	{
+		for (i = 0; i < 200; i++)
+		{
+			A += char(i + 0x25);
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << "  in i = " << i << std::endl;
+	}
+	std::cout << "sizeof class A: " << A.get_pow() << std::endl;
+	std::cout << "Printing class A: " << A << std::endl;
+
+	std::cout << "Creating class B by power" << std::endl;
+	SymbAm B(40);
+	std::cout << "sizeof class B: " << B.get_pow() << std::endl;
+	std::cout << "Printing class B: " << B << std::endl;
+
+	std::cout << "Creating class C by concatination of A and B" << std::endl;
+	SymbAm C;
+	try
+	{
+		C = A + B;
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	
+	std::cout << "sizeof class C: " << C.get_pow() << std::endl;
+	std::cout << "Printing class C: " << C << std::endl;
+
+	std::cout << "Creating class D by string" << std::endl;
+	SymbAm D("1234567890-=qwefjsk");	
+	std::cout << "sizeof class D: " << D.get_pow() << std::endl;
+	std::cout << "Printing class D: " << D << std::endl;
+
+	std::cout << "class C is concatination of A and B" << std::endl;
+	try
+	{
+		C = D + B;
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	std::cout << "sizeof class C: " << C.get_pow() << std::endl;
+	std::cout << "Printing class C: " << C << std::endl;
+}
+
---
+++ SymbAm.cpp
@@ -0,0 +1,289 @@
+#include "SymbAm.h"
+
+
+bool SymbAm::find_as(char a) const
+{
+	for (int i = 0; i < this->power; i++)
+	{
+		if (this->array[i] == a)
+			return true;
+	}
+	return false;
+}
+
+void SymbAm::clean_str()
+{
+	memset(this->array, 0x00, sizeof(this->array) * sizeof(char));
+}
+
+SymbAm::SymbAm()
+{
+
+}
+
+SymbAm::SymbAm(char s)
+{
+	this->power = 1;
+	this->array[0] = s;
+}
+
+SymbAm::SymbAm(int p)
+{
+	if (p >= M)
+	{
+		throw std::exception("Too big power");
+	}
+	else
+	{
+		this->power = p;
+		for (int i = 0; i < p; i++)
+		{
+			this->array[i] = 0x20 + i;
+		}
+	}
+		
+}
+
+SymbAm::SymbAm(char * came, int len)
+{
+	if (len >= M)
+	{
+		throw std::exception("Too big string");
+	}
+	else
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (find_as(came[i]))
+			{
+				throw std::exception("There is some repeated symbols");
+			}
+				
+			else
+			{
+				this->power++;
+				this->array[i] = came[i];
+			}
+				
+		}
+	}
+}
+
+SymbAm::SymbAm(const char * came)
+{
+	if (strlen(came) >= M)
+	{
+		throw std::exception("Too big string");
+	}
+	else
+	{
+		for (int i = 0; i < strlen(came); i++)
+		{
+			if (find_as(came[i]))
+			{
+				throw std::exception("There is some repeated symbols");
+			}
+
+			else
+			{
+				this->power++;
+				this->array[i] = came[i];
+			}
+
+		}
+	}
+}
+
+SymbAm::SymbAm(const SymbAm & A)
+{
+	memcpy(this->array, A.get_arr(), M * sizeof(char));
+	this->power = A.get_pow();
+}
+
+int SymbAm::get_pow() const
+{
+	return this->power;
+}
+
+const char * SymbAm::get_arr() const
+{
+	return this->array;
+}
+
+int SymbAm::find(char a) const
+{
+	for (int i = 0; i < this->power; i++)
+	{
+		if (this->array[i] == a)
+			return i;
+	}
+	return -1;
+}
+
+SymbAm::~SymbAm()
+{
+
+}
+
+
+std::ostream & operator<<(std::ostream & out, const SymbAm & point)
+{
+	for (int i = 0; i < point.get_pow(); i++)
+	{
+		out << point.array[i];
+	}
+	out << std::endl;
+	return out;
+}
+
+std::istream & operator>>(std::istream & in, SymbAm & point)
+{
+	std::string inp;
+
+	in >> inp;
+
+	if (inp.length() >= M)
+	{
+		throw std::exception("Too big string");
+	}
+
+	for (int i = 0; i < inp.length(); i++)
+	{
+		if (point.find_as(inp[i]))
+		{
+			throw std::exception("There is some repeated symbols");
+		}
+		else
+		{
+			point.power++;
+			point.array[i] = inp[i];
+		}
+	}
+
+	return in;
+}
+
+SymbAm operator+(const SymbAm & d1, const SymbAm & d2)
+{
+	SymbAm ncl(d1);
+	int am = d1.get_pow() + d2.get_pow();
+
+	for (int i = d1.get_pow(), j = 0; j < d2.power; i++, j++)
+	{
+		if (!d1.find_as(d2.array[j]))
+		{
+			ncl.array[i] = d2.array[j];
+			ncl.power++;
+			if(ncl.power >= M)
+				throw std::exception("Out of range");
+		}
+		else
+		{
+			i--;
+			continue;
+		}
+			
+	}
+	return ncl;
+}
+
+SymbAm operator+(const char &s, const SymbAm & d2)
+{
+	SymbAm S(s);
+	SymbAm nw = S + d2;
+	return nw;
+}
+
+SymbAm operator-(const SymbAm & d1, const SymbAm & d2)
+{
+	SymbAm ncl;
+	int am = d1.get_pow() + d2.get_pow();
+
+	for (int i = 0, j = 0; i < d1.get_pow(); i++)
+	{
+		if (d2.power <= i)
+		{
+			for (int k = i; k < d1.get_pow(); k++)
+			{
+				ncl.array[j] = d1.array[k];
+				ncl.power++;
+				j++;
+			}
+			break;
+		}
+			
+		if (d1.find_as(d2.array[i]))
+			continue;
+		else
+		{
+			ncl.array[j] = d1.array[i];
+			ncl.power++;
+			j++;
+		}
+	}
+
+	return ncl;
+}
+
+SymbAm operator-(const char &s, const SymbAm & d2)
+{
+	SymbAm S(s);
+	SymbAm nw = S - d2;
+	return nw;
+}
+
+SymbAm operator*(const SymbAm & d1, const SymbAm & d2)
+{
+	SymbAm ncl;
+	int am = d1.get_pow() + d2.get_pow();
+
+	for (int i = 0, j = 0; i < d1.get_pow(); i++)
+	{
+		if (d2.power <= i)
+		{
+			for (int k = 0; k < d2.power; k++)
+			{
+				if (d2.find_as(d1.array[i]))
+					continue;
+				else
+				{
+					ncl.array[j] = d2.array[k];
+					ncl.power++;
+					j++;
+				}
+			}
+			break;
+		}
+			
+		if (!d1.find_as(d2.array[i]))
+			continue;
+		else
+		{
+			ncl.array[j] = d1.array[i];
+			ncl.power++;
+			j++;
+		}
+	}
+	return ncl;
+}
+
+SymbAm operator*(const char &s, const SymbAm & d2)
+{
+	SymbAm S(s);
+	SymbAm nw = S * d2;
+	return nw;
+}
+
+SymbAm & SymbAm::operator+=(const char & right)
+{
+	if (this->power >= M)
+	{
+		throw std::exception("Out of range");
+	}
+	else
+	{
+		this->array[this->power] = right;
+		this->power++;
+	}
+	return *this;
+}
